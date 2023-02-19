52d231991f1a818211e2e8d8c27215babbba2129
Leonid <leonid0991@gmail.com>
2023-02-19 15:16:56 +0200

    scramblies

--- scramblies.js
+++ scramblies.js
@@ -1,3 +1,5 @@
+// kata - https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+
 function scramble(str1, str2) {
     let count = Object.create(null);
 
@@ -9,3 +11,8 @@ function scramble(str1, str2) {
         return count[a]--;
     });
 }
+
+
+scramble('rkqodlw', 'world') // True
+scramble('cedewaraaossoqqyt', 'codewars') // True
+scramble('katas', 'steak') // False
